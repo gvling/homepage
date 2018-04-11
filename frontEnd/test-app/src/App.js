a62b1cf51c2aa1f048e7531943e727e870d2aee7
May <gvling@163.com>
2018-04-11 12:10:20 +0000

    add scroll event listener to App.js

--- frontEnd/test-app/src/App.js
+++ frontEnd/test-app/src/App.js
@@ -1,21 +1,115 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import classnames from 'classnames';
+import Card, { CardHeader, CardMedia, CardContent, CardActions } from 'material-ui/Card';
+import Collapse from 'material-ui/transitions/Collapse';
+import IconButton from 'material-ui/IconButton';
+import Typography from 'material-ui/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Zoom from 'material-ui/transitions/Zoom';
+import IMG_FF7 from './assets/images/ff7_1.jpg';
 
-class App extends Component {
+const styles = theme => ({
+  card: {
+    maxWidth: '100%',
+    height: '100%',
+  },
+  media: {
+    height: '70%',
+  },
+  actions: {
+    display: 'flex',
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+    marginLeft: 'auto',
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  }
+});
+
+class RecipeReviewCard extends React.Component {
+  state = { expanded: false,
+            zoomOut: true,
+          };
+
+  componentDidMount() {
+      window.addEventListener('scroll', this.handleScroll);
+      console.log('add scroll event listener');
+  };
+
+  componentWillUnmount() {
+      window.removeEventListener('scroll', this.handleScroll);
+      console.log('remove scroll event listener');
+  };
+
+  handleScroll = (event) => {
+      let scrollTop = event.srcElement.body.scrollTop,
+          itemTranslate = Math.min(0, scrollTop/3 - 60);
+    console.log(itemTranslate)
+  };
+  handleExpandClick = () => {
+    this.setState({ expanded: !this.state.expanded });
+  };
+
   render() {
+    const { classes } = this.props;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{height: '100%'}}>
+        <Zoom in={this.state.zoomOut}>
+          <Card className={classes.card}>
+            <CardHeader
+              action={
+                <IconButton>
+                  <MoreVertIcon />
+                </IconButton>
+              }
+            />
+            <CardMedia
+              className={classes.media}
+              image={IMG_FF7}
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography component="p">
+                hogehoge
+              </Typography>
+            </CardContent>
+            <CardActions className={classes.actions} disableActionSpacing>
+              <IconButton
+                className={classnames(classes.expand, {
+                  [classes.expandOpen]: this.state.expanded,
+                })}
+                onClick={this.handleExpandClick}
+                aria-expanded={this.state.expanded}
+                aria-label="Show more"
+              >
+                <ExpandMoreIcon />
+              </IconButton>
+            </CardActions>
+            <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography paragraph variant="body2">
+                  fugafuga
+                </Typography>
+              </CardContent>
+            </Collapse>
+          </Card>
+        </Zoom>
       </div>
     );
   }
 }
 
-export default App;
+RecipeReviewCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(RecipeReviewCard);
