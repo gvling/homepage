f3a72b1b302c2b899b101ce17f9ae39ddb30f781
gvling <gvling@163.com>
2019-06-23 19:12:28 +0900

    init success

--- frontEnd/webpack.config.js
+++ frontEnd/webpack.config.js
@@ -1,6 +1,7 @@
- publidDir = __dirname + '/public';
+publidDir = __dirname + '/public';
 
 module.exports = {
+  mode: "development",
   entry: [
     './src/index.js'
   ],
@@ -11,21 +12,25 @@ module.exports = {
   },
   module: {
     rules: [{
-      // enforce: 'pre'がついていないローダーより早く処理が実行される
-      // babel-loaderで変換する前にコードを検証したいため、指定が必要
-      enforce: "pre",
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      use: [{
-        loader: 'eslint-loader'
-      }]
-    }, {
       test: /\.jsx?$/,
       exclude: /node_modules/,
       use: [{
         loader: 'babel-loader',
         options: {
           presets: ['@babel/preset-react', '@babel/preset-env']
+        }
+      }]
+    },{
+      // enforce: 'pre'がついていないローダーより早く処理が実行される
+      // babel-loaderで変換する前にコードを検証したいため、指定が必要
+      enforce: "pre",
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'eslint-loader',
+        options: {
+          formatter: require("eslint-friendly-formatter"),
+          fix: true
         }
       }]
     }]
